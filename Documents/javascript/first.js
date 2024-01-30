fd48b502b071e6252e99b1455d059f514810e142
Akshay Vaghela <akshayvaghela2763@gmail.com>
2024-01-30 13:11:57 +0530

    ok

---
+++ Documents/javascript/first.js
@@ -0,0 +1,135 @@
+// var a = 5 + "10";
+// var b = 9 - "5";
+// var c = "java" + "script";
+// var d = "2" + "3"
+// var e = " " + 0;
+// var f = "vinod" - "thapa";
+// var g = true + true;
+// var h = true + false;
+// var i = false + true;
+// var j = false - true;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+// console.log(i);
+// console.log(j);
+
+// console.log(21==21);
+// console.log(21===21);
+
+// var x=3;
+// console.log(x++);
+// x=3;
+// console.log(++x);
+
+// console.log(3**3);
+
+// function sum(a,b=2)
+// {
+//     return total=a+b;
+// }
+
+// console.log(sum());
+
+// functions
+
+// function sum(a,b)
+//     {
+//         return total=a+b;
+//     }
+
+// var ans=sum(5,6)    
+// console.log(ans);
+
+// anonymous function
+
+// var ans=function(a,b)
+// {
+//     return total=a+b;
+// }
+// // console.log(ans(5,10));
+// var sum=ans(5,15)
+// console.log('the sum is '+ sum)
+
+// const and let keyword
+
+// function sum(){
+//     var a=5;
+//     var b=10;
+//     console.log(a+b);
+//     if(true)
+//     {
+//         var ans=10+a+b;
+//         console.log(ans);
+//     }
+
+// }
+// console.log(sum());
+
+// function sum(){
+//     let a=5;
+//     let b=10;
+//     console.log(a+b);
+//     if(true)
+//     {
+//         let ans=10+a+b;
+//         console.log(ans);
+//     }
+//     console.log(ans);
+// }
+// console.log(sum());
+
+// function sum(){
+//     const a=5;
+//     const b=10;
+//     console.log(a+b);
+//     if(true)
+//     {
+//         const ans=10+a+b;
+//         console.log(ans);
+//     }
+//     console.log(ans);
+// }
+// console.log(sum());
+
+// flat arrow function
+
+// const sum = () => `sum of two number is ${(a=5)+(b=4)}`
+// console.log(sum())
+
+// arrays
+
+// var name1=['Akshay','Smit','Shiv']
+ // for(i=0;i<name1.length;i++)
+ // {
+ //     console.log(name1[i]);
+ // 
+ // for(let elements in name1)
+ // {
+ //     console.log(elements);
+ // 
+ // for(let elements of name1)
+ // {
+ //     console.log(elements);
+ // }
+
+// name1.forEach((elements,index,array) => {
+//     console.log(elements);
+// })
+
+// var myFriendNames = ["vinod","bahadur","thapa","thapatechnical","thapa"];
+
+// console.log(myFriendNames.indexOf("thapa", -10  ));
+
+// Date and time object
+
+// console.log(new Date());
+// console.log(new Date().toLocaleDateString());
+// console.log(new Date().toString());
+// console.log(Date.now());
